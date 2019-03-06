e5be304decadd282e9d7a34cdaa5fa195070d8df
kuanke <asdqsczser@gmail.com>
2019-03-06 14:54:45 +0800

    debug

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -15,7 +15,8 @@ import simplejson as json
 import os
 from multiprocessing import Process, JoinableQueue
 import multiprocessing
-import zlib
+from base64 import b64decode
+from base64 import b64encode
 
 # EncodeMap = {'Pass': 1, 'Skill': 2, 'Shoot': 3, 'Conversion': 4, 'Block': 5, 'Steal': 6, 'Rebound': 7,'ShootCancel':8}
 
@@ -70,8 +71,8 @@ def extraction_info2(info):
     elif id == 'ShootResult':
         return '#'.join([str(x) for x in [id,info['score'],info['shoot_type'],'None']])
     elif id == 'GameEnd':
-        #json.loads(zlib.decompress(x).decode())
-        return id+'#'+zlib.compress(json.dumps(info).encode('utf-8'))+'#'+info['role_score']+'#'+info['game_score']
+        #json.loads(b64decode(x).decode())
+        return id+'#'+b64encode(json.dumps(info).encode('utf-8'))+'#'+info['role_score']+'#'+info['game_score']
     else:
         return id+'#None#None#None'
 
